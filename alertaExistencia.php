524aff653710dadb8e2a063624af79d8ceb6d86a
Daniel Paniagua <dpaniagua0@gmail.com>
2012-02-28 13:50:10 -0600

    #10 se comenzo la notificacion baja existencia de producto

--- alertaExistencia.php
+++ alertaExistencia.php
@@ -1,3 +1,8 @@
+<?php
+include("config.php");
+include("lock.php");
+
+?>
 <div id="message-container" class="order-message alert alert-warning " style="text-align: center"> 
   <a data-dismiss="alert" href="#" class="close">&times;</a>
   <p id="message" class="message"> El producto tal se esta terminando.</p>
